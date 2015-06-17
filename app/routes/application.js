cf2710397dab340172704444d8188a5de3f94dce
Vitaliy Ribachenko <vit@ribachenko.com>
2015-06-17 10:23:13 +0300

    added scss and fixed table retreiving

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,23 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model () {
-    return this.store.findAll('table');
+    return $.ajax('http://localhost:3000/');
+    //return this.store.findAll('table');
+  },
+
+  setupController (controller, model) {
+    var fields = model.fields.map((field) => field.name);
+    var tables = [];
+
+    model.tables.forEach((table) => {
+      fields.forEach((field) => {
+        tables.push({
+          name: table[field]
+        });
+      });
+    });
+
+    controller.set('tables', tables);
   }
 
 });
